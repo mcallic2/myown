ede515fce3bbb6244fefc6b9e0bab6f24c162290
Caitlyn Mcallister <caitlyn.mcallister@noaa.gov>
2024-04-19 11:46:24 -0400

    add test case

---
+++ src/animals/test_animals/test_cats.F90
@@ -0,0 +1,12 @@
+program im_testing_cats
+  implicit none
+
+  character(len=5) :: test_cat_kind
+  real             :: test_claws
+  logical          :: test_petting
+
+  test_cat_kind = "tabby"
+  test_claws    = 3.0
+  test_petting  = .false.
+
+end program im_testing_cats
