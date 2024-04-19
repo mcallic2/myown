962512b69f100381958280ece8942841a5e7d84e
Caitlyn Mcallister <caitlyn.mcallister@noaa.gov>
2024-04-19 11:30:41 -0400

    space

--- src/animals/the_routines.f
+++ src/animals/the_routines.f
@@ -7,7 +7,7 @@
         character(len=*) :: dog_kind
 
         fur_type%color = "black"
-        print *, "the kind of dog is", dog_kind
+        print *, "the kind of dog is a", dog_kind
         print *, "this dog has", fur_type%color, "fur"
 
       end subroutine dogs
