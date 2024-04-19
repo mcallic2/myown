31ae1b404c6a0404e6cc09e496948c91ae9e3a22
Caitlyn Mcallister <caitlyn.mcallister@noaa.gov>
2024-04-19 11:29:00 -0400

    calling routine in main program

--- src/animals/main_program.f
+++ src/animals/main_program.f
@@ -1,8 +1,14 @@
       program beginning
 
+        use characteristics
+
         implicit none
 
+        character(len=*) :: this_dog
+
+        this_dog = "german shepard"
         print *, "this is the main program"
+        call dogs(this_dog)
 
 
       end program beginning
